fe81b2b415f848007b7d31cbec1e77524bf35ee2
lee-ji-hoon <dlwlgns1240@naver.com>
2022-02-09 22:11:16 +0900

    회원 Entity 생성

---
+++ src/main/java/com/community/account/Account.java
@@ -0,0 +1,49 @@
+package com.community.account;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private String studentId;
+
+    private LocalDateTime joinedAt;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+
+    public void generateEmailCheckToken() {
+        this.emailCheckToken = UUID.randomUUID().toString();
+    }
+
+    public void completeSignUp() {
+        this.emailVerified = true;
+        this.joinedAt = LocalDateTime.now();
+    }
+
+    public boolean isValidToken(String token) {
+        return this.emailCheckToken.equals(token);
+    }
+}
