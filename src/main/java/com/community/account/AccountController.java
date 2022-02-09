228cc5a27fccb5e915ad23e307597349d5f19ea0
lee-ji-hoon <dlwlgns1240@naver.com>
2022-02-09 22:10:58 +0900

    회원 컨트롤러 생성 -> 로그인 페이지는 미완성 회원가입시 자동로그인

--- src/main/java/com/community/account/AccountController.java
+++ src/main/java/com/community/account/AccountController.java
@@ -2,14 +2,63 @@ package com.community.account;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 public class AccountController {
 
-    @GetMapping("/")
-    public String mainPage() {
-        return "index";
+    private final SignUpFormValidator signUpFormValidator;
+    private final AccountService accountService;
+    private final AccountRepository accountRepository;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
     }
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model) {
+        model.addAttribute(new SignUpForm());
+        return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        Account account = accountService.processNewAccount(signUpForm);
+        accountService.login(account);
+        return "redirect:/";
+    }
+
+    @GetMapping("/check-email-token")
+    public String checkEmailToken(String token, String email, Model model) {
+        Account account = accountRepository.findByEmail(email);
+
+        if (account == null) {
+            model.addAttribute("error", "wrong.email");
+            return "account/checked-email";
+        }
+
+        if (!account.isValidToken(token)) {
+            model.addAttribute("error", "wrong.token");
+            return "account/checked-email";
+        }
+
+        accountService.completeSignUp(account);
+        model.addAttribute("numberOfUser", accountRepository.count());
+        model.addAttribute("nickname", account.getNickname());
+        return "account/checked-email";
+    }
+
 }
