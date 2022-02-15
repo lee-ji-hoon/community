0d2686e80ea55446c0a0c6078853bfb68e8b4745
lee-ji-hoon <dlwlgns1240@naver.com>
2022-02-16 01:54:02 +0900

    이름 변경

--- src/main/java/com/community/profile/validator/NicknameValidator.java
+++ src/main/java/com/community/profile/validator/NicknameValidator.java
@@ -2,7 +2,7 @@ package com.community.profile.validator;
 
 import com.community.account.Account;
 import com.community.account.AccountRepository;
-import com.community.profile.form.NicknameForm;
+import com.community.profile.form.AccountForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -16,12 +16,12 @@ public class NicknameValidator implements Validator {
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return NicknameForm.class.isAssignableFrom(clazz);
+        return AccountForm.class.isAssignableFrom(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
-        NicknameForm nicknameForm = (NicknameForm) target;
+        AccountForm nicknameForm = (AccountForm) target;
         Account byNickname = accountRepository.findByNickname(nicknameForm.getNickname());
         if (byNickname != null) {
             errors.rejectValue("nickname", "wrong.value", "입력하신 닉네임을 사용할 수 없습니다.");
