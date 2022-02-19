f00d2a71788de1ac83318dadc6eb905345075898
lee-ji-hoon <dlwlgns1240@naver.com>
2022-02-19 22:46:13 +0900

    스터디원 조회

--- src/main/java/com/community/study/StudyController.java
+++ src/main/java/com/community/study/StudyController.java
@@ -28,12 +28,35 @@ public class StudyController {
     private final StudyFormValidator studyFormValidator;
     private final StudyRepository studyRepository;
 
+    private static final String STUDY_FORM = "/study-form";
+    private static final String STUDY_VIEW = "study/form";
+
+    private static final String STUDY_PATH = "study/{path}";
+
     @InitBinder("studyForm")
     public void studyFormInitBinder(WebDataBinder webDataBinder) {
         webDataBinder.addValidators(studyFormValidator);
     }
 
-    @GetMapping("study/{path}")
+    @GetMapping(STUDY_FORM)
+    public String newStudyForm(@CurrentUser Account account, Model model) {
+        model.addAttribute(account);
+        model.addAttribute(new StudyForm());
+        return STUDY_VIEW;
+    }
+
+    @PostMapping(STUDY_FORM)
+    public String newStudySubmit(@CurrentUser Account account, @Valid StudyForm studyForm, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            return STUDY_VIEW;
+        }
+
+        Study newStudy = studyService.createNewStudy(modelMapper.map(studyForm, Study.class), account);
+        return "redirect:/study/" + URLEncoder.encode(newStudy.getPath(), StandardCharsets.UTF_8);
+    }
+
+    @GetMapping(STUDY_PATH)
     public String viewStudy(@CurrentUser Account account, @PathVariable String path, Model model) {
         model.addAttribute(account);
         model.addAttribute(studyRepository.findByPath(path));
@@ -41,21 +64,13 @@ public class StudyController {
         return "study/view";
     }
 
-    @GetMapping("/new-study")
-    public String newStudyForm(@CurrentUser Account account, Model model) {
+    @GetMapping(STUDY_PATH + "/members")
+    public String viewStudyMembers(@CurrentUser Account account, @PathVariable String path, Model model) {
         model.addAttribute(account);
-        model.addAttribute(new StudyForm());
-        return "study/form";
+        model.addAttribute(studyRepository.findByPath(path));
+
+        return "study/members";
     }
 
-    @PostMapping("/new-study")
-    public String newStudySubmit(@CurrentUser Account account, @Valid StudyForm studyForm, Errors errors) {
-        if (errors.hasErrors()) {
-            return "study/form";
-        }
-
-        Study newStudy = studyService.createNewStudy(modelMapper.map(studyForm, Study.class), account);
-        return "redirect:/study/" + URLEncoder.encode(newStudy.getPath(), StandardCharsets.UTF_8);
-    }
 
 }
