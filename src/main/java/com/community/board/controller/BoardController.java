b11bcfed5fa3f14d4d82d444b479719d7934c861
Jwhyee <kawnsdud@naver.com>
2022-03-07 02:44:14 +0900

    index 수치 조정 및 H2 DB 연동

--- src/main/java/com/community/board/controller/BoardController.java
+++ src/main/java/com/community/board/controller/BoardController.java
@@ -26,6 +26,8 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
