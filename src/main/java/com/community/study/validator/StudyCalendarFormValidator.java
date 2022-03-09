0336427c854b980ee832e68dd390eccd4bfd1b3e
lee-ji-hoon <dlwlgns1240@naver.com>
2022-03-09 22:25:49 +0900

    링크 오류 해결

--- src/main/java/com/community/study/validator/StudyCalendarFormValidator.java
+++ src/main/java/com/community/study/validator/StudyCalendarFormValidator.java
@@ -1,6 +1,7 @@
 package com.community.study.validator;
 
 import com.community.study.form.StudyCalendarForm;
+import com.community.study.form.StudyForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -19,21 +20,21 @@ public class StudyCalendarFormValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-        StudyCalendarForm studyCalendarForm = (StudyCalendarForm)target;
+        StudyCalendarForm studyForm = (StudyCalendarForm)target;
 
-        if(!isNotValidLimitMemberDate(studyCalendarForm)){
+        if(!isNotValidLimitMemberDate(studyForm)){
             errors.rejectValue("limitMemberDate", "wrong.date", "인원 모집 종료 일시를 다시 확인해주세요.");
         }
 
-        if (!isNotValidLimitStudyDate(studyCalendarForm)) {
+        if (!isNotValidLimitStudyDate(studyForm)) {
             errors.rejectValue("limitStudyDate", "wrong.date", "스터디 종료일을 다시 확인해주세요");
         }
 
-        if (!isNotValidStartStudyDate(studyCalendarForm)) {
+        if (!isNotValidStartStudyDate(studyForm)) {
             errors.rejectValue("startStudyDate", "wrong.date", "스터디 시작일을 다시 확인해주세요.");
         }
 
-        if (studyCalendarForm.getLimitMember() < 2 && studyCalendarForm.getLimitMember() > 30) {
+        if (studyForm.getLimitMember() < 2 && studyForm.getLimitMember() > 30) {
             errors.rejectValue("limitMember", "wrong.member", "인원은 2~30명 사이로 선택해주세요");
         }
     }
