98c7c7556533033d6129cbd913c4b485ee62e810
lee-ji-hoon <dlwlgns1240@naver.com>
2022-03-29 20:50:15 +0900

    testData 버그 수정

--- src/main/java/com/community/TestDataInit.java
+++ src/main/java/com/community/TestDataInit.java
@@ -67,15 +67,15 @@ public class TestDataInit {
             /*boardRepository.save(new Board(null, "자유", "", "오늘 학식은 뭔가요?", null, null, null, false, 0,BOARD_CONTENT_VALUE, accountRepository.findByEmail("dlwlgns1240@naver.com"), 0, LocalDateTime.now().minusHours(1), null));*/
         }
 
-        /*if (!accountRepository.existsByStudentId("17-100425")) {
+        if (!accountRepository.existsByStudentId("17-100425")) {
             accountRepository.save(new Account(null, "abc@nsu.ac.kr", "tester1", "17-100425", "테스터", passwordEncoder.encode("12401240"),
                     true, "asdf1", null, null, LocalDateTime.now().minusHours(1), LocalDateTime.now().minusHours(1),
                     "잘 부탁드립니다", "https://github.com/lee-ji-hoon", "대학생", "서울, 용산", null, null, null, null, true, true,
                     true, true, true, true, null, 0));
-            *//*boardRepository.save(new Board(null, "정보", "java", "스프링에 대한 설명", "스프링은 과연 무엇인가..", null, null, false, 0,FORUM_TEST_CONTENT_VALUE3, accountRepository.findByEmail("test1@naver.com"), 0, LocalDateTime.now().minusHours(1), null));*//*
-        }*/
+//            boardRepository.save(new Board(null, "정보", "java", "스프링에 대한 설명", "스프링은 과연 무엇인가..", null, null, false, 0,FORUM_TEST_CONTENT_VALUE3, accountRepository.findByEmail("test1@naver.com"), 0, LocalDateTime.now().minusHours(1), null));
+        }
 
-        if (!accountRepository.existsByStudentId("17-100378")) {
+        if (!accountRepository.existsByStudentId("17-100444")) {
             accountRepository.save(new Account(null, "kawnsdud@nsu.ac.kr", "jwhy", "17-100444", "준영", passwordEncoder.encode("test1234!"),
                     true, "asdf12", null, null, LocalDateTime.now().minusHours(1), LocalDateTime.now().minusHours(1),
                     "hello", "https://github.com/Jwhyee", "대학생", "경기도, 수원", null, null, null, null, true, true,
