70df3c44e33fcde56e70811759f808c6a42ab052
Jwhyee <kawnsdud@naver.com>
2022-03-30 19:28:25 +0900

    쪽지 전체 읽기 ajax 및 로직 추가

--- src/main/java/com/community/web/controller/ChatController.java
+++ src/main/java/com/community/web/controller/ChatController.java
@@ -64,6 +64,25 @@ public class ChatController {
         return "";
     }
 
+    // 전체 읽음 확인
+    @ResponseBody
+    @RequestMapping(value = "/chat/allReadChk")
+    public void allReadCheck(@CurrentUser Account account) {
+        List<Room> myRoom = new ArrayList<>();
+        List<Room> roomAttender = roomRepository.findByRoomAttenderOrderByLastSendTimeDesc(account);
+        List<Room> roomHost = roomRepository.findByRoomHostOrderByLastSendTimeDesc(account);
+        myRoom.addAll(roomHost);
+        myRoom.addAll(roomAttender);
+
+        for (Room room : myRoom) {
+            List<Chat> unReadChat = chatRepository.findByRoomAndReadChk(room, false);
+            for (Chat chat : unReadChat) {
+                chat.setReadChk(true);
+                chatRepository.save(chat);
+            }
+        }
+    }
+
     // 채팅방에서 쪽지 보내기
     @ResponseBody
     @RequestMapping(value = "/chat/send")
