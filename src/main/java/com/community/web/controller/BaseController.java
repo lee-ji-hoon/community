b94a17f546e41a478f8f2a45d2779051c9315f13
Jwhyee <kawnsdud@naver.com>
2022-03-30 18:26:10 +0900

    쪽지 알림 기능 구현

--- src/main/java/com/community/web/controller/BaseController.java
+++ src/main/java/com/community/web/controller/BaseController.java
@@ -1,5 +1,18 @@
 package com.community.web.controller;
 
+import com.community.domain.account.Account;
+import com.community.domain.account.AccountRepository;
+import com.community.domain.account.CurrentUser;
+import com.community.domain.alarm.AlarmRepository;
+import com.community.domain.chat.Chat;
+import com.community.domain.chat.ChatRepository;
+import com.community.domain.chat.Room;
+import com.community.domain.chat.RoomRepository;
+import com.community.service.AlarmService;
+import com.community.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,12 +21,23 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.w3c.dom.events.EventException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
+@Slf4j
+@RequiredArgsConstructor
 public class BaseController {
+
+    private final RoomRepository roomRepository;
+    private final ChatRepository chatRepository;
+    private final AccountRepository accountRepository;
+    private final AlarmRepository alarmRepository;
+    private final ChatService chatService;
+    private final AlarmService alarmService;
+
     /*@ExceptionHandler({EventException.class, RuntimeException.class})
     public String eventErrorHandler(EventException exception, Model model) {
         model.addAttribute("message","event error");
@@ -29,14 +53,28 @@ public class BaseController {
     }*/
 
     //ModelAttribute 또다른 용번
-// @Controller 또는 @ControllerAdvice를 사용한 클래에스 모델 정보를 초기화할 때 사용한다.
+    // @Controller 또는 @ControllerAdvice를 사용한 클래에스 모델 정보를 초기화할 때 사용한다.
     @ModelAttribute()
-    public void categories(Model model) {
-        Map<Long, String> mapnew = new HashMap<>();
-        mapnew.put(1L, "하이1");
-        mapnew.put(2L, "하이2");
-        mapnew.put(3L, "하이3");
+    public void globalChatNotify(Model model, @CurrentUser Account account) {
+        // 현재 계정이 참여중인 Room을 찾는 로직
+        List<Room> findMyRooms = new ArrayList<>();
+        List<Room> roomHostByAccount = roomRepository.findByRoomHostOrderByLastSendTimeDesc(account);
+        List<Room> roomAttenderByAccount = roomRepository.findByRoomAttenderOrderByLastSendTimeDesc(account);
 
-        model.addAttribute("categories", mapnew);
+        // 해당 Room의 ChatList를 가져오는 로직
+        List<Chat> chatNotifyLists = new ArrayList<>();
+        findMyRooms.addAll(roomAttenderByAccount);
+        findMyRooms.addAll(roomHostByAccount);
+
+        for (Room findMyRoom : findMyRooms) {
+            List<Chat> readChkFalseChat = chatRepository.findByRoomAndReadChk(findMyRoom, false);
+            for (Chat chat : readChkFalseChat) {
+                if (!account.getNickname().equals(chat.getSender().getNickname())) {
+                    chatNotifyLists.add(chat);
+                }
+            }
+        }
+        model.addAttribute("g_chatNotify", chatNotifyLists);
+        model.addAttribute("g_myRoom", findMyRooms);
     }
 }
