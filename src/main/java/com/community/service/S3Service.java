18d579d1b8f7fb2c7533c5aa412963ca84a437c5
lee-ji-hoon <dlwlgns1240@naver.com>
2022-04-02 21:51:22 +0900

    aws s3에 market image 저장 성공 -> image 유형에 안맞는 경우 front단에서 잡아야함

---
+++ src/main/java/com/community/service/S3Service.java
@@ -0,0 +1,72 @@
+package com.community.service;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@NoArgsConstructor
+public class S3Service {
+    private AmazonS3 s3Client;
+
+    // @Value lombok 아님
+    // perperties 값 가져옴
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @PostConstruct
+    public void setS3Client() {
+        // 자격증명
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials)) // 자격 증명 객체 얻기
+                .withRegion(this.region) // client갖고옴
+                .build();
+    }
+
+    public String upload(MultipartFile file) throws IOException, IllegalArgumentException {
+        String fileName = createFileName(file.getOriginalFilename());
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+
+        // 업로드를 위해 사용되는 함수 (참고 https://docs.aws.amazon.com/ko_kr/AmazonS3/latest/userguide/upload-objects.html)
+        s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), objectMetadata)
+                .withCannedAcl(CannedAccessControlList.PublicRead)); // 외부에 공개되는 이미지이므로 read 권한 주기
+        return s3Client.getUrl(bucket, fileName).toString(); // 업로드 후 해당 URL DB에 저장할 수 있도록 컨트롤러에 저장
+    }
+
+    // 기존 확장자는 유지한채 유니크한 파일 이름 생성 로직
+    private String createFileName(String originalFilename) {
+        return UUID.randomUUID().toString().concat(getFileExtension(originalFilename));
+    }
+
+    // 파일의 확자명을 가져오는 로직
+    private String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(String.format("잘못된 형식의 파일 ($s) 입니다", fileName));
+        }
+    }
+}
