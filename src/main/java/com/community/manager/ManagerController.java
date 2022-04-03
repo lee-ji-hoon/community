ebc1a1480121dd8939c95e77d2e9f4312417652e
Jwhyee <kawnsdud@naver.com>
2022-04-03 23:30:17 +0900

    Manager View 수정

--- src/main/java/com/community/manager/ManagerController.java
+++ src/main/java/com/community/manager/ManagerController.java
@@ -1,8 +1,11 @@
 package com.community.manager;
 
+import com.community.domain.account.Account;
+import com.community.domain.account.CurrentUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
@@ -11,22 +14,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ManagerController {
 
     @GetMapping("/manager/main")
-    public String managerMain() {
+    public String managerMain(Model model) {
         return "manager/m_main";
     }
 
+    @GetMapping("/manager/board")
+    public String managerBoard(Model model) {
+        return "manager/m_board";
+    }
+
     @GetMapping("/manager/setting")
-    public String managerSetting() {
+    public String managerSetting(Model model) {
         return "manager/settings";
     }
 
     @GetMapping("/manager/account")
-    public String managerTables() {
-        return "manager/account";
+    public String managerTables(Model model) {
+        return "manager/tables";
     }
 
     @GetMapping("/manager/maps")
-    public String managerMaps() {
+    public String managerMaps(Model model) {
         return "manager/maps";
     }
 }
