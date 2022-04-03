0a0ed803328f676de4e124f4f52163c953a4d680
Jwhyee <kawnsdud@naver.com>
2022-04-03 22:37:59 +0900

    Manager Controller 생성

---
+++ src/main/java/com/community/manager/ManagerController.java
@@ -0,0 +1,32 @@
+package com.community.manager;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ManagerController {
+
+    @GetMapping("/manager/main")
+    public String managerMain() {
+        return "manager/m_main";
+    }
+
+    @GetMapping("/manager/setting")
+    public String managerSetting() {
+        return "manager/settings";
+    }
+
+    @GetMapping("/manager/tables")
+    public String managerTables() {
+        return "manager/tables";
+    }
+
+    @GetMapping("/manager/maps")
+    public String managerMaps() {
+        return "manager/maps";
+    }
+}
