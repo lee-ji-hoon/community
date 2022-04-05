7d1e9aeb3b353bbe6cf8921b3a12c0ce34f4cf10
lee-ji-hoon <dlwlgns1240@naver.com>
2022-04-05 16:17:55 +0900

    profile image -> aws 서버 저장으로 변경

--- src/main/java/com/community/service/S3Service.java
+++ src/main/java/com/community/service/S3Service.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
