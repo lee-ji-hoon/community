41c995cb6cca746071ef3c0d261614ca699049a8
lee-ji-hoon <dlwlgns1240@naver.com>
2022-04-06 01:28:49 +0900

    쪽지 테스트

--- src/main/java/com/community/Community.java
+++ src/main/java/com/community/Community.java
@@ -3,11 +3,23 @@ package com.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Community {
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Community.class, args);
+
+		SpringApplication application = new SpringApplication(Community.class);
+		application.setLogStartupInfo(false);
+		application.run(args);
 	}
 
 }
