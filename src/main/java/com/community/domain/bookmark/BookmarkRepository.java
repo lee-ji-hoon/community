d0e0e6e88a103d768c5302680786138669690c26
Jwhyee <kawnsdud@naver.com>
2022-04-15 18:21:28 +0900

    Bookmark Entity 생성

---
+++ src/main/java/com/community/domain/bookmark/BookmarkRepository.java
@@ -0,0 +1,7 @@
+package com.community.domain.bookmark;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+}
