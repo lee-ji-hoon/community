de4970a5d85bbaae40c100f37c27e09566ff2835
Jwhyee <kawnsdud@naver.com>
2022-04-15 22:54:01 +0900

    Entity BookmarkList 추가

--- src/main/java/com/community/domain/council/Council.java
+++ src/main/java/com/community/domain/council/Council.java
@@ -1,12 +1,16 @@
 package com.community.domain.council;
 
 import com.community.domain.account.Account;
+import com.community.domain.board.Reply;
+import com.community.domain.bookmark.Bookmark;
 import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -38,6 +42,12 @@ public class Council {
     @ManyToOne(targetEntity = Account.class, fetch = FetchType.LAZY)
     @JoinColumn(name = "postWriter")
     private Account postWriter;
+
+    @OneToMany(mappedBy = "council", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reply> replyList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "council", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Bookmark> bookmarkList = new ArrayList<>();
 
     // bucket object 이름
     private String fileName;
