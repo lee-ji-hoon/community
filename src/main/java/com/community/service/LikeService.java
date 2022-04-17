8c5b9add28b845b3489faead00266e092bfc3542
Jwhyee <kawnsdud@naver.com>
2022-04-17 14:28:37 +0900

    Like 로직 전체 변경

--- src/main/java/com/community/service/LikeService.java
+++ src/main/java/com/community/service/LikeService.java
@@ -3,6 +3,8 @@ package com.community.service;
 import com.community.domain.account.Account;
 import com.community.domain.board.Board;
 import com.community.domain.board.BoardRepository;
+import com.community.domain.council.Council;
+import com.community.domain.council.CouncilRepository;
 import com.community.domain.likes.LikeRepository;
 import com.community.domain.likes.Likes;
 import lombok.RequiredArgsConstructor;
@@ -17,22 +19,57 @@ import java.util.List;
 public class LikeService {
     private final LikeRepository likeRepository;
     private final BoardRepository boardRepository;
+    private final CouncilRepository councilRepository;
 
-    public Likes addLike(Account account, Long boardId) {
-        Board board = boardRepository.findById(boardId).orElseThrow();
+    public Likes addLike(Account account, String postSort, Long postId) {
+        Likes likes = Likes.builder()
+                .account(account)
+                .build();
 
-        //중복 좋아요 방지
-        if(isNotAlreadyLike(account, board)) {
-            Likes save = likeRepository.save(new Likes(board, account));
-            return save;
+        switch (postSort) {
+            case "board":
+                Board currentBoard = boardRepository.findByBid(postId);
+                // 중복 좋아요 방지
+                if(isNotAlreadyBoardLike(account, currentBoard)) {
+                    likes.setBoard(currentBoard);
+                    return likeRepository.save(likes);
+                }
+            case "council":
+                Council currentCouncil = councilRepository.findByCid(postId);
+                // 중복 좋아요 방지
+                if(isNotAlreadyCouncilLike(account, currentCouncil)) {
+                    likes.setCouncil(currentCouncil);
+                    return likeRepository.save(likes);
+                }
         }
-        return null;
+        return likes;
     }
 
+    public void deleteLike(Account account, String postSort, Long postId) {
+        switch (postSort) {
+            case "board":
+                Board currentBoard = boardRepository.findByBid(postId);
+                // 중복 좋아요 방지
+                if(isNotAlreadyBoardLike(account, currentBoard)) {
+                    Likes likes = likeRepository.findByBoardAndAccount(currentBoard, account);
+                    likeRepository.delete(likes);
+                }
+            case "council":
+                Council currentCouncil = councilRepository.findByCid(postId);
+                // 중복 좋아요 방지
+                if(isNotAlreadyCouncilLike(account, currentCouncil)) {
+                    Likes likes = likeRepository.findByCouncilAndAccount(currentCouncil, account);
+                    likeRepository.delete(likes);
+                }
+        }
+    }
 
-
-    private boolean isNotAlreadyLike(Account account, Board board) {
+    private boolean isNotAlreadyBoardLike(Account account, Board board) {
         return likeRepository.findByAccountAndBoard(account, board).isEmpty();
+    }
+
+    private boolean isNotAlreadyCouncilLike(Account account, Council council) {
+        return likeRepository.findByAccountAndCouncil(account, council).isEmpty();
     }
 
     public int boardLikeSize(Board board) {
