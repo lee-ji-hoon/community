754f7abb0058d27721edaa410db0658bbc873230
lee-ji-hoon <dlwlgns1240@naver.com>
2022-04-20 21:29:40 +0900

    테스트 데이터 수정

--- src/main/java/com/community/TestDataInit.java
+++ src/main/java/com/community/TestDataInit.java
@@ -135,7 +135,7 @@ public class TestDataInit {
                     null, 5, 10));
         }*/
 
-        for (int i = 0; i < 20; i++) {
+        /*for (int i = 0; i < 20; i++) {
             Graduation graduation = Graduation.builder()
                     .title(i + make)
                     .teamName(make)
@@ -143,7 +143,7 @@ public class TestDataInit {
                     .path(make)
                     .graduationType("WEB")
                     .teamMember("이지훈")
-              /*      .graduationDate(2019)
+                    .graduationDate(2019)
                     .build();
             graduationRepository.save(graduation);
         }
