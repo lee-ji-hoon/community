8c56c8f281a42539f2b5902bb3a78eae6a300e28
lee-ji-hoon <dlwlgns1240@naver.com>
2022-04-20 17:24:40 +0900

    페이징 QueryDsl

---
+++ src/main/java/com/community/domain/graduation/GraduationRepositoryExtension.java
@@ -0,0 +1,13 @@
+package com.community.domain.graduation;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public interface GraduationRepositoryExtension {
+
+    Page<Graduation> findByGraduationDate(int date, Pageable pageable);
+
+    Page<Graduation> findAll(Pageable pageable);
+}
