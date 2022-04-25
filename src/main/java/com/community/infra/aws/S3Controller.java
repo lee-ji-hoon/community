b4552fdb02efc9e11949ddcb1a137c4c6cec7c4d
lee-ji-hoon <dlwlgns1240@naver.com>
2022-04-25 19:18:41 +0900

    이미지 삭제 버그 해결

--- src/main/java/com/community/infra/aws/S3Controller.java
+++ src/main/java/com/community/infra/aws/S3Controller.java
@@ -2,6 +2,7 @@ package com.community.infra.aws;
 
 import com.community.service.GraduationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,21 +10,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class S3Controller {
 
     private final S3Repository s3Repository;
     private final S3Service s3Service;
 
     @ResponseBody
-    @RequestMapping(value = "/s3/image/delete", method = RequestMethod.POST)
-    public ResponseEntity graduationDeleteImage(@RequestParam(value = "imageName") String imageName) {
-        S3 s3 = s3Repository.findByImageName(imageName);
+    @RequestMapping(value = "/s3/image/delete", method = RequestMethod.GET)
+    public ResponseEntity graduationDeleteImage(@RequestParam(value = "imageId", required = false) int imageId) {
+        log.info("s3 id 값 : {}", imageId);
+
+        Optional<S3> byId = s3Repository.findById(Long.valueOf(imageId));
+        S3 s3 = byId.get();
 
         s3Service.deleteImage(s3);
-
-        if(s3 != null) ResponseEntity.badRequest().build();
 
         return ResponseEntity.ok().build();
     }
