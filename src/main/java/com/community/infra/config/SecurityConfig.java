27bf271179a4bb4806613c4f99278c78be9ca8df
Jwhyee <kawnsdud@naver.com>
2022-05-20 18:52:21 +0900

    매니저페이지 권한을 주기 위해 여러 옵션 수정 및 추가

--- src/main/java/com/community/infra/config/SecurityConfig.java
+++ src/main/java/com/community/infra/config/SecurityConfig.java
@@ -28,19 +28,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .mvcMatchers("/login", "/logout", "/sign-up", "/check-email", "/check-email-token",
-                        "/email-login", "/check-email-login", "/login-link", "/email-login-view", "/study/*").permitAll()
+                        "/email-login", "/check-email-login", "/login-link", "/email-login-view").permitAll()
+                .mvcMatchers("/study/*", "/board/*", "/council/*", "/").authenticated()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
-                .anyRequest().authenticated()
-                .antMatchers("/manager/**").hasAnyRole("ADMIN");
-        http.exceptionHandling().accessDeniedPage("/error-page");
+                .antMatchers("/manager/*").hasAnyRole("ADMIN");
+        /*
+        * 기존 formLogin 로직
         http.formLogin()
                 .loginPage("/login")
                 .permitAll();
+        */
         http.logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/");
+                .logoutSuccessUrl("/login");
+
+        // 인증 필요시 로그인 페이지와 로그인 성공시 리다이랙팅 경로 지정
+        http.formLogin().loginPage("/login").defaultSuccessUrl("/", true);
+        // 로그인이 수행될 uri 매핑 (post 요청이 기본)
+        http.formLogin().loginProcessingUrl("/login").defaultSuccessUrl("/", true);
+        //http.exceptionHandling().accessDeniedPage("/error-page");
+
+        http.userDetailsService(accountService);
+
         http.rememberMe()
-                .userDetailsService(accountService)
+                //.userDetailsService(accountService)
                 .tokenRepository(tokenRepository());
     }
 
