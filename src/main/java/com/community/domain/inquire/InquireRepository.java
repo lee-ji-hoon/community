c77b835522d51e54bf7d705b24ba3749c3bb63b6
Jwhyee <kawnsdud@naver.com>
2022-05-22 02:52:33 +0900

    InquireRepository 쿼리 추가

--- src/main/java/com/community/domain/inquire/InquireRepository.java
+++ src/main/java/com/community/domain/inquire/InquireRepository.java
@@ -1,11 +1,17 @@
 package com.community.domain.inquire;
 
+import com.community.domain.account.Account;
+import com.community.domain.board.Reply;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface InquireRepository extends JpaRepository<Inquire, Long> {
 
-    List<Inquire> findByIsAnsweredOrderByUploadTimeDesc(Boolean isAnswered);
+    List<Inquire> findByIsAnsweredAndAccountOrderByUploadTimeDesc(Boolean isAnswered, Account account);
+
+    Page<Inquire> findByIsAnsweredOrderByUploadTimeDesc(Boolean isAnswered, Pageable pageable);
 
 }
